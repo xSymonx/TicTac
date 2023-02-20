1d02f981b850fbdf3adef1edc7f1aac9da2cba1c
xSymonx <81527636+xSymonx@users.noreply.github.com>
2023-02-20 20:28:36 +0100

    Add files via upload

---
+++ TicTac/TicTac/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TicTacToe
+{
+    public partial class MainWindow : Window
+    {
+        private TicTacToe game;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Initialize the game
+            game = new TicTacToe();
+            game.GameEnded += OnGameEnded;
+
+            // Set button click events
+            Button00.Click += OnButtonClick;
+            Button01.Click += OnButtonClick;
+            Button02.Click += OnButtonClick;
+            Button10.Click += OnButtonClick;
+            Button11.Click += OnButtonClick;
+            Button12.Click += OnButtonClick;
+            Button20.Click += OnButtonClick;
+            Button21.Click += OnButtonClick;
+            Button22.Click += OnButtonClick;
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Get the button that was clicked
+            Button button = (Button)sender;
+
+            // Get the row and column of the button
+            int row = Grid.GetRow(button);
+            int column = Grid.GetColumn(button);
+
+            try
+            {
+                // Make a move in the game
+                game.MakeMove(row, column);
+
+                // Update the button content
+                button.Content = game.Board[row, column];
+
+                // Update the player text
+                PlayerText.Text = game.CurrentPlayer.ToString();
+
+                // Check if the game has ended
+                if (game.GameOver)
+                {
+                    // Show the end panel
+                    EndPanel.Visibility = Visibility.Visible;
+
+                    // Set the end text
+                    if (game.Winner != null)
+                    {
+                        EndText.Text = game.Winner + " wins!";
+                    }
+                    else
+                    {
+                        EndText.Text = "Tie game!";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Show an error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnGameEnded(object sender, EventArgs e)
+        {
+            // Show the end panel
+            EndPanel.Visibility = Visibility.Visible;
+
+            // Set the end text
+            if (game.Winner != null)
+            {
+                EndText.Text = game.Winner + " wins!";
+            }
+            else
+            {
+                EndText.Text = "Tie game!";
+            }
+        }
+
+        private void NewGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Reset the game
+            game.Reset();
+
+            // Reset the button content
+            Button00.Content = "";
+            Button01.Content = "";
+            Button02.Content = "";
+            Button10.Content = "";
+            Button11.Content = "";
+            Button12.Content = "";
+            Button20.Content = "";
+            Button21.Content = "";
+            Button22.Content = "";
+
+            // Update the player text
+            PlayerText.Text = game.CurrentPlayer.ToString();
+
+            // Hide the end panel
+            EndPanel.Visibility = Visibility.Collapsed;
+        }
+    }
+}
+
---
+++ TicTac/TicTac/TicTacToe.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Numerics;
+
+namespace TicTacToe
+{
+    public class TicTacToe
+    {
+        public string[,] Board { get; private set; }
+        public Player CurrentPlayer { get; private set; }
+        public bool GameOver { get; private set; }
+        public string Winner { get; private set; }
+
+        public event EventHandler GameEnded;
+
+        public TicTacToe()
+        {
+            // Initialize the board
+            Board = new string[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Board[i, j] = "";
+                }
+            }
+
+            // Set the current player
+            CurrentPlayer = Player.X;
+
+            // Reset the game
+            Reset();
+        }
+
+        public void MakeMove(int row, int column)
+        {
+            // Check if the game is over
+            if (GameOver)
+            {
+                throw new InvalidOperationException("The game is over.");
+            }
+
+            // Check if the field is already occupied
+            if (Board[row, column] != "")
+            {
+                throw new InvalidOperationException("The field is already occupied.");
+            }
+
+            // Make the move
+            Board[row, column] = CurrentPlayer.ToString();
+
+            // Check if the game is
+            // Check for a win
+            if (CheckForWin())
+            {
+                GameOver = true;
+                Winner = CurrentPlayer.ToString();
+                OnGameEnded();
+                return;
+            }
+
+            // Check for a tie
+            if (CheckForTie())
+            {
+                GameOver = true;
+                OnGameEnded();
+                return;
+            }
+
+            // Switch to the next player
+            if (CurrentPlayer == Player.X)
+            {
+                CurrentPlayer = Player.O;
+            }
+            else
+            {
+                CurrentPlayer = Player.X;
+            }
+        }
+
+        public void Reset()
+        {
+            // Reset the board
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Board[i, j] = "";
+                }
+            }
+
+            // Reset the game state
+            CurrentPlayer = Player.X;
+            GameOver = false;
+            Winner = null;
+        }
+
+        private bool CheckForWin()
+        {
+            // Check the rows
+            for (int i = 0; i < 3; i++)
+            {
+                if (Board[i, 0] != "" && Board[i, 0] == Board[i, 1] && Board[i, 1] == Board[i, 2])
+                {
+                    return true;
+                }
+            }
+
+            // Check the columns
+            for (int i = 0; i < 3; i++)
+            {
+                if (Board[0, i] != "" && Board[0, i] == Board[1, i] && Board[1, i] == Board[2, i])
+                {
+                    return true;
+                }
+            }
+
+            // Check the diagonals
+            if (Board[0, 0] != "" && Board[0, 0] == Board[1, 1] && Board[1, 1] == Board[2, 2])
+            {
+                return true;
+            }
+            if (Board[0, 2] != "" && Board[0, 2] == Board[1, 1] && Board[1, 1] == Board[2, 0])
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool CheckForTie()
+        {
+            // Check if all fields are occupied
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (Board[i, j] == "")
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        protected virtual void OnGameEnded()
+        {
+            EventHandler handler = GameEnded;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public enum Player
+    {
+        X,
+        O
+    }
+}
